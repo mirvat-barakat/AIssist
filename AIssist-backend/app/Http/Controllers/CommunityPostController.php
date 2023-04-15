6796c7bd3b9162a8066a33aaa427001235367dd3
unknown <mirvatbarakat1@gmail.com>
2023-04-15 16:49:15 +0300

    backend<>Added createPost API

--- AIssist-backend/app/Http/Controllers/CommunityPostController.php
+++ AIssist-backend/app/Http/Controllers/CommunityPostController.php
@@ -4,8 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class CommunityPostController extends Controller
 {
-    //
+    public function createPost(Request $request)
+    {
+        $post = new Post;
+        $post->user_id = $request->user()->id;
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Post created successfully'
+        ]);
+    }
 }
