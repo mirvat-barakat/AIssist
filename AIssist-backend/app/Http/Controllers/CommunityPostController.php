ba90a47b75c1586b56d49ad626cd2e056ecbc19b
unknown <mirvatbarakat1@gmail.com>
2023-04-15 17:22:48 +0300

    backend<>Added deletePost API

--- AIssist-backend/app/Http/Controllers/CommunityPostController.php
+++ AIssist-backend/app/Http/Controllers/CommunityPostController.php
@@ -7,10 +7,9 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Post;
 
-class CommunityPostController extends Controller
-{
-    public function createPost(Request $request)
-    {
+class CommunityPostController extends Controller{
+    
+    public function createPost(Request $request){
         $post = new Post;
         $post->user_id = $request->user()->id;
         $post->title = $request->input('title');
@@ -23,12 +22,36 @@ class CommunityPostController extends Controller
         ]);
     }
 
-    public function getPosts()
-    {
+    public function getPosts(){
         $posts = DB::table('posts')->get();
         return response()->json([
             'status' => 'success',
             'users' => $posts
+        ]);
+    }
+
+    public function delete(Request $request, $id){
+        $post = Post::find($id);
+
+        if (!$post) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Post not found'
+            ], 404);
+        }
+
+        $user = $request->user();
+        if (!$user->is_admin && $user->id !== $post->user_id) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        $post->delete();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Post deleted successfully'
         ]);
     }
 }
