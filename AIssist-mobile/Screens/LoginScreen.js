2f46f107abe8e57b151578dfc0bd38f012e8a13d
unknown <mirvatbarakat1@gmail.com>
2023-04-17 23:30:26 +0300

    AIssist-mobile<>Added login screen style sheet

--- AIssist-mobile/Screens/LoginScreen.js
+++ AIssist-mobile/Screens/LoginScreen.js
@@ -38,3 +38,54 @@ export default function LoginScreen() {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F08080',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    logo: {
+        width: 250,
+        height: 250,
+        resizeMode: 'contain',
+        marginTop: -10,
+      },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        marginTop:-20,
+      },
+    inputContainer: {
+        width: 250,
+        marginBottom: 20,
+      },
+    inputLabel: {
+        marginBottom: 10,
+        fontSize:20,
+      },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        backgroundColor:'#FFFFFF',
+        borderRadius: 10,
+        padding: 10,
+        fontSize: 16,
+      },
+    button: {
+        backgroundColor: '#FFC6C6',
+        borderRadius: 10,
+        padding: 12,
+        alignItems: 'center',
+        width: 100,
+        marginBottom: 10,
+        marginTop:10,
+      },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+      },
+
+});
