df5a435e4c8d895f85f3bb91d9dd23734ee706b4
unknown <mirvatbarakat1@gmail.com>
2023-04-20 04:35:22 +0300

    AIssist-mobile<>Imported header component into home screen

--- AIssist-mobile/App.js
+++ AIssist-mobile/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AuthStack from './src/navigation/AuthStack';
+import HomeScreen from './src/screens/Home';
 
 function App() {
   return (
-    <NavigationContainer>
-      <AuthStack />
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <AuthStack />
+    // </NavigationContainer>
+    <View>
+      <HomeScreen/>
+    </View>
   );
 }
 
---
+++ AIssist-mobile/src/screens/Home/index.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { Text, TextInput, TouchableOpacity, View, SafeAreaView, Image } from 'react-native';
+import styles from './styles';
+import Header from '../../components/Header';
+
+export default function HomeScreen() {
+    return(
+        <View>
+            <Header/>
+        </View>
+    );
+}
