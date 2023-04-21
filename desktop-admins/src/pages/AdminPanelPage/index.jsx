6106c737ca472eb0bc0a558af9da80c00ed2cbf6
unknown <mirvatbarakat1@gmail.com>
2023-04-21 23:44:43 +0300

    AIssist-desktop<> Added admin panel page and imported header to it

---
+++ desktop-admins/src/pages/AdminPanelPage/index.jsx
@@ -0,0 +1,9 @@
+import React , {useState, useEffect} from "react";
+import Header from  '../../Components/Admin/Header';
+
+const AdminPage = () => {
+    return (
+        <Header/>
+    );
+}
+export default AdminPage;
