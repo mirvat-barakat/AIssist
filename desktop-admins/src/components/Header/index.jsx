7f8f6f8fb84c99f6a0c0b277676b6515d63ca212
unknown <mirvatbarakat1@gmail.com>
2023-04-21 23:39:14 +0300

    AIssist-desktop<> Added handleLogout function

--- desktop-admins/src/components/Header/index.jsx
+++ desktop-admins/src/components/Header/index.jsx
@@ -3,11 +3,35 @@ import AppButton from "../../Button";
 import "./style.css";
 
 const Header = () => {
+
+    const handleLogout = () => {
+
+        const token = localStorage.getItem("token");
+        
+        localStorage.clear();
+        
+        const logout = {
+          method: 'POST',
+          url: 'http://127.0.0.1:8000/api/v0.0.1/logout/',
+          headers: {
+            'content-type': 'application/json',
+            'Accept' : 'application/json',
+            'Authorization': 'bearer ' + token
+          },
+        };
+        
+        axios.request(logout)
+            .then(response => {
+                navigate("/");
+          });
+      }
+
+      
     return (
         <header >
           <nav className='navbar'>
             <div ><h1>Admin Pannel</h1></div>
-            <AppButton button_name="Logout" />
+            <AppButton button_name="Logout" onClick={handleLogout} />
           </nav>
         </header>
       );
