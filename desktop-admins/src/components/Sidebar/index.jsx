84bb416a19773c57d06165ef785a34edbcd3de6e
unknown <mirvatbarakat1@gmail.com>
2023-04-22 00:01:24 +0300

    AIssist-desktop<> Added sidebar component

---
+++ desktop-admins/src/components/Sidebar/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./style.css";
+
+const Sidebar = () => {
+    return (
+        <aside>
+            <div className='sidebar-links'>
+            <a href='#' className="link" >View Users</a>
+            </div>
+        </aside>
+    );
+};
+
+export default Sidebar
