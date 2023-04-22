093c9aef47d012ec94484b4263c1c8c804e52511
unknown <mirvatbarakat1@gmail.com>
2023-04-22 03:38:03 +0300

    AIssist-desktop<> Added navigation links to sidebar

--- desktop-admins/src/components/Sidebar/index.jsx
+++ desktop-admins/src/components/Sidebar/index.jsx
@@ -6,8 +6,16 @@ const Sidebar = () => {
     return (
         <aside className='sidebar'>
             <img src={logo} alt="logo" className="logo"></img>
-            <div className='sidebar-links'>
-            <a href='#' className="link" >View Users</a>
+            <div className='items'>
+                <div className='items-list'>
+                    <ion-icon name="people-outline" ></ion-icon>
+                    <a href="#"> <span className="ml-3">Users Accounts</span></a>
+                </div>
+             
+             <a href="#"><span className="ml-3">Specialists Managment</span></a>
+             <a href="#"><span className="ml-3">Community Moderation</span></a>
+             <a href="#"> <span className="ml-3">User Data Analysis</span></a>
+             <a href="#"><span className="ml-3">Rewards Managment</span></a>
             </div>
         </aside>
     );
