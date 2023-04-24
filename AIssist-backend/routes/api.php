7e387d88ed9c2c2bccf63f8b2432c8cc59fce131
unknown <mirvatbarakat1@gmail.com>
2023-04-24 07:41:08 +0300

    AIssist-backend<> Added route for add specialist API

--- AIssist-backend/routes/api.php
+++ AIssist-backend/routes/api.php
@@ -19,7 +19,8 @@ Route::group(["prefix" => "v0.0.1"], function()
     Route::group(["middleware" => ["auth:api"]], function()
     {
         Route::get('/users', [UserController::class, 'getUsers']);
-
+        
+        Route::post('/specialist', [SpecialistsController::class, 'addSpecialist']);
         Route::get('/specialists', [SpecialistsController::class, 'getSpecialists']);
         Route::get('/specialists/search', [SpecialistsController::class, 'searchSpecialists']);
         Route::get('/specialists/search-by-category', [SpecialistsController::class, 'searchSpecialistsByCategory']);
