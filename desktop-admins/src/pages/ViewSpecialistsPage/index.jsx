3af60b487823ea9b016b620ac494e32f3decc0da
unknown <mirvatbarakat1@gmail.com>
2023-04-24 08:29:45 +0300

    AIssist-desktop<>Added view specialists page

---
+++ desktop-admins/src/pages/ViewSpecialistsPage/index.jsx
@@ -0,0 +1,8 @@
+import React , {useState, useEffect} from "react";
+import Header from  '../../components/Header';
+import Sidebar from "../../components/Sidebar";
+import "./styles.css";
+
+const ViewSpecialists = () => {}
+
+export default ViewPage;
