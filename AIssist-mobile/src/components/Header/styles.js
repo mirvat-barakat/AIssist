1e8a085e4000f342afa19f793051cb9296abd413
unknown <mirvatbarakat1@gmail.com>
2023-04-28 01:59:42 +0300

    AIssist-mobile<>Adjusted header style sheet

--- AIssist-mobile/src/components/Header/styles.js
+++ AIssist-mobile/src/components/Header/styles.js
@@ -5,21 +5,22 @@ const styles = StyleSheet.create({
     header:{
         left:0,
         right:0,
-        height:90,
+        height:80,
         backgroundColor:colors.primary_1,
         flexDirection:"row",
         justifyContent:"space-between",
+        alignItems: 'center',
         elevation:4,
     },
     logo1:{
         top:5,
-        width: 80,
-        height: 65,
+        width: 90,
+        height: 80,
         resizeMode: 'contain',
     },
     icon: {
         top:10,
-        left:-20,
+        paddingRight:20,
         color: colors.white,
     },
 });
