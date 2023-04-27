461e1b051894909ceb99039b611abb489b60a5e9
unknown <mirvatbarakat1@gmail.com>
2023-04-28 02:12:35 +0300

    AIssist-mobile<>Added header1 component structure

---
+++ AIssist-mobile/src/components/Header1/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import styles from './styles';
+
+const Header1 = ({ title, onPressBackButton }) => {
+
+    return (
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.backButton} onPress={onPressBackButton}>
+            <Ionicons name="ios-arrow-back" size={24} color="white" />
+          </TouchableOpacity>
+          <View style={styles.titleContainer}>
+             <Text style={styles.title}>{title}</Text>
+          </View>
+        </View>
+      );
+};
+
+export default Header1;
