d85f1a7773a1b55de3b92784decb6c2202edd9b1
unknown <mirvatbarakat1@gmail.com>
2023-04-28 14:41:05 +0300

    AIssist-mobile<>Added activities form request

--- AIssist-mobile/src/screens/Activities/index.jsx
+++ AIssist-mobile/src/screens/Activities/index.jsx
@@ -9,6 +9,53 @@ export default function ActivitiesScreen() {
     return(
         <SafeAreaView style={styles.mainView}>
             <Header/>
+            <View>
+                <View>
+                <Text style={styles.inputLabel}> Name</Text>
+                 <TextInput
+                  style={styles.input}
+                 />
+                </View>
+                <View>
+                <Text style={styles.inputLabel}> Age</Text>
+                 <TextInput
+                  style={styles.input}
+                 />
+                </View>
+                <View>
+                <Text style={styles.inputLabel}> Gender</Text>
+                 <TextInput
+                  style={styles.input}
+                 />
+                </View>
+                <View>
+                <Text style={styles.inputLabel}> Diagnosis</Text>
+                 <TextInput
+                  style={styles.input}
+                 />
+                </View>
+                <View>
+                <Text style={styles.inputLabel}> Medications</Text>
+                 <TextInput
+                  style={styles.input}
+                 />
+                </View>
+                <View>
+                <Text style={styles.inputLabel}> What are your child's interests</Text>
+                 <TextInput
+                  style={styles.input}
+                 />
+                </View>
+                <View>
+                <Text style={styles.inputLabel}> Is there anything else you'd like us to know about your child?</Text>
+                 <TextInput
+                  style={styles.input}
+                 />
+                </View>
+                <TouchableOpacity style={styles.button} >
+                 <Text style={styles.buttonText}>Save</Text>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 };
