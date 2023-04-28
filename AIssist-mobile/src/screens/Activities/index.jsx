9ac84dedc5b56cdd7bc3d67afcd7e3c306db612e
unknown <mirvatbarakat1@gmail.com>
2023-04-28 14:50:33 +0300

    AIssist-mobile<>Added styles for form

--- AIssist-mobile/src/screens/Activities/index.jsx
+++ AIssist-mobile/src/screens/Activities/index.jsx
@@ -9,45 +9,45 @@ export default function ActivitiesScreen() {
     return(
         <SafeAreaView style={styles.mainView}>
             <Header/>
-            <View>
-                <View>
-                <Text style={styles.inputLabel}> Name</Text>
+            <View style={styles.mainFormActivities}>
+                <View style={styles.formActivities}>
+                <Text style={styles.inputLabel1}> Name</Text>
                  <TextInput
                   style={styles.input}
                  />
                 </View>
                 <View>
-                <Text style={styles.inputLabel}> Age</Text>
+                <Text style={styles.inputLabel1}> Age</Text>
                  <TextInput
                   style={styles.input}
                  />
                 </View>
                 <View>
-                <Text style={styles.inputLabel}> Gender</Text>
+                <Text style={styles.inputLabel1}> Gender</Text>
                  <TextInput
                   style={styles.input}
                  />
                 </View>
                 <View>
-                <Text style={styles.inputLabel}> Diagnosis</Text>
+                <Text style={styles.inputLabel1}> Diagnosis</Text>
                  <TextInput
                   style={styles.input}
                  />
                 </View>
                 <View>
-                <Text style={styles.inputLabel}> Medications</Text>
+                <Text style={styles.inputLabel1}> Medications</Text>
                  <TextInput
                   style={styles.input}
                  />
                 </View>
                 <View>
-                <Text style={styles.inputLabel}> What are your child's interests</Text>
+                <Text style={styles.inputLabel1}> What are your child's interests</Text>
                  <TextInput
                   style={styles.input}
                  />
                 </View>
                 <View>
-                <Text style={styles.inputLabel}> Is there anything else you'd like us to know about your child?</Text>
+                <Text style={styles.inputLabel1}> Is there anything else you'd like us to know about your child?</Text>
                  <TextInput
                   style={styles.input}
                  />
--- AIssist-mobile/src/screens/Activities/styles.js
+++ AIssist-mobile/src/screens/Activities/styles.js
@@ -4,7 +4,46 @@ import { colors } from '../../constants/Palette';
 const styles = StyleSheet.create({
     mainView:{
         width:'100%',
+        backgroundColor:'#F2F4F4',
     },
+    mainFormActivities:{
+        display:'flex',
+        flexDirection:'column',
+        alignItems:'center',
+    },
+    formActivities:{
+        display:'flex',
+        flexDirection:'column',
+        alignItems:'flex-start',
+    },
+    inputLabel1:{
+        color:'black',
+        fontWeight:'600'
+    },
+    button: {
+        backgroundColor: colors.primary_3,
+        borderRadius: 10,
+        padding: 10,
+        alignItems: 'center',
+        width: 250,
+        marginBottom: 10,
+        marginTop:10,
+      },
+    buttonText: {
+        color: colors.white,
+        fontSize: 18,
+      },
+      inputLabel: {
+        marginBottom: 10,
+        fontSize:20,
+        color:colors.white,
+      },
+    input: {
+        backgroundColor: colors.white,
+        borderRadius: 10,
+        padding: 10,
+        fontSize: 16,
+      },
 });
 
 export default styles;
