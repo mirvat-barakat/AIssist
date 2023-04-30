d3c26f4fd4c5c17869d5f35aa264c33d1f0bb1ae
unknown <mirvatbarakat1@gmail.com>
2023-04-30 03:48:07 +0300

    AIssist-desktop<>Updated sidebar icons style

--- desktop-admins/src/components/Sidebar/index.jsx
+++ desktop-admins/src/components/Sidebar/index.jsx
@@ -10,22 +10,21 @@ const Sidebar = () => {
         <aside className='sidebar'>
             <img src={logo} alt="logo" className="logo"></img>
             <div className='items'>
-
             <div className='sidebar-links'>
-                <div className='category'>
-                    <a href="#" className='link'><FontAwesomeIcon icon={faUser} className="ml-2" /><span className="ml-3">Users Accounts</span></a>
+                <div>
+                    <a href="#" className='link'><FontAwesomeIcon icon={faUser} className="faicon" /><span className="ml-3">Users Accounts</span></a>
                 </div>
                 <div className='category'>
-                    <a href="#" className='link'><FontAwesomeIcon icon={faUserTie} className="ml-2" /><span className="ml-3">Specialists Managment</span></a>
+                    <a href="#" className='link'><FontAwesomeIcon icon={faUserTie} className="faicon" /><span className="ml-3">Specialists Managment</span></a>
                 </div>
                 <div className='category'>
-                    <a href="#" className='link'><FontAwesomeIcon icon={faUsersCog} className="ml-2" /><span className="ml-3">Community Moderation</span></a>
+                    <a href="#" className='link'><FontAwesomeIcon icon={faUsersCog} className="faicon" /><span className="ml-3">Community Moderation</span></a>
                 </div>
                 <div className='category'>
-                    <a href="#" className='link'><FontAwesomeIcon icon={faChartLine} className="ml-2" /> <span className="ml-3">User Data Analysis</span></a>
+                    <a href="#" className='link'><FontAwesomeIcon icon={faChartLine} className="faicon" /> <span className="ml-3">User Data Analysis</span></a>
                 </div>
                 <div className='category'>
-                    <a href="#" className='link'><FontAwesomeIcon icon={faGift} className="ml-2" /><span className="ml-3">Rewards Managment</span></a>
+                    <a href="#" className='link'><FontAwesomeIcon icon={faGift} className="faicon" /><span className="ml-3">Rewards Managment</span></a>
                 </div>
             </div>
             </div>
