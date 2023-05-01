941e3d1d79f32d8cc3ffe818a64a7b58d61081e4
unknown <mirvatbarakat1@gmail.com>
2023-05-01 22:29:10 +0300

    AIssist-desktop<>Updated stylesheet

--- desktop-admins/src/components/Form/index.jsx
+++ desktop-admins/src/components/Form/index.jsx
@@ -8,49 +8,49 @@ const Form = () => {
         <form className="form">
           <label htmlFor="name" className="input1-text">Name:</label>
           <input
-          className="input1"
+            className="input1"
             type="text"
             id="name"
             required
           />
           <label htmlFor="email" className="input1-text">Email:</label>
           <input
-          className="input1"
+            className="input1"
             type="email"
             id="email"
             required
           />
           <label htmlFor="category" className="input1-text">Category:</label>
           <input
-          className="input1"
+            className="input1"
             type="text"
             id="category"
             required
           />
           <label htmlFor="speciality" className="input1-text">Speciality:</label>
           <input
-          className="input1"
+            className="input1"
             type="text"
             id="speciality"
             required
           />
           <label htmlFor="phone-number" className="input1-text">Phone Number:</label>
           <input
-          className="input1"
+            className="input1"
             type="text"
             id="phone-number"
             required
           />
           <label htmlFor="location" className="input1-text">Location:</label>
           <input
-          className="input1"
+            className="input1"
             type="text"
             id="location"
             required
           />
           <label htmlFor="profile-picture" className="input1-text">Profile Picture:</label>
           <input
-          className="input1"
+            className="input1"
             type="file"
             id="profile-picture"
           />
