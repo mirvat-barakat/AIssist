2398b759e374886dc92c6126e11582039d164758
unknown <mirvatbarakat1@gmail.com>
2023-05-01 22:37:05 +0300

    AIssist-desktop<>Updated stylesheet

--- desktop-admins/src/App.js
+++ desktop-admins/src/App.js
@@ -4,7 +4,6 @@ import { Routes, Route, BrowserRouter as Router, Switch } from "react-router-dom
 import LoginPage from './pages/LoginPage';
 import AdminPannelPage from './pages/AdminPanelPage';
 import ViewUsersPage from './pages/ViewUsersPage';
-import AddSpecialistsPage from './pages/AddSpecialistsPage';
 import ViewSpecialistsPage from './pages/ViewSpecialistsPage';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         <Route path="/" element={<LoginPage  />} />
         <Route path="/admin" element={<AdminPannelPage />} />
         <Route path="/view_users" element={<ViewUsersPage />} />
-        <Route path="/add_specialists" element={<AddSpecialistsPage />} />
         <Route path="/view_specialists" element={<ViewSpecialistsPage />} />
         <Route path="*" element={<div>404</div>} />
     </Routes>
