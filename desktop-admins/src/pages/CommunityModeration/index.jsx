ac92de6c8c13d9d3c915a4eba5bf68e98dce0ca1
unknown <mirvatbarakat1@gmail.com>
2023-05-02 22:20:51 +0300

    AIssist-desktop<>Added get posts function

--- desktop-admins/src/pages/CommunityModeration/index.jsx
+++ desktop-admins/src/pages/CommunityModeration/index.jsx
@@ -5,6 +5,30 @@ import "./styles.css";
 
 const Community = () => {
 
+    const[posts, setPosts]= useState([]);
+    const token = localStorage.getItem("token");
+
+    const getPosts = {
+        method: 'GET',
+        url: 'http://192.168.1.6:8000/api/v0.0.1/community/posts',
+        headers: {
+          'content-type': 'application/json',
+          'Accept' : 'application/json',
+          'Authorization': 'bearer ' + token
+        },
+      };
+  
+      useEffect(() => {
+        axios.request(getPosts)
+            .then(response => {
+                console.log(response);
+                setPosts(response.data.posts);
+            })
+            .catch(function (error) {
+                console.error();
+            });
+      },[token]);
+
     return(
         <>
         <div className="body">
