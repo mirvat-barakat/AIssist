1c73a1032981a87ef9497dd66cce6ebce97ae01f
unknown <mirvatbarakat1@gmail.com>
2023-05-02 22:11:53 +0300

    AIssist-desktop<>Added community moderation page

---
+++ desktop-admins/src/pages/CommunityModeration/index.jsx
@@ -0,0 +1,8 @@
+import React , {useState, useEffect} from "react";
+import Sidebar from "../../components/Sidebar";
+import axios from "axios";
+import "./styles.css";
+
+const Community = () => {};
+
+export default Community;
