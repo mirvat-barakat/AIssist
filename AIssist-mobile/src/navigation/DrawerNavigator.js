c161f1cf1df345693c13a595fe927c6f5c9f4671
unknown <mirvatbarakat1@gmail.com>
2023-05-03 03:40:01 +0300

    AIssist-mobile<>Added drawer navigation file

---
+++ AIssist-mobile/src/navigation/DrawerNavigator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import CommunityScreen from '../screens/Community';
+import ActivitiesScreen from '../screens/Activities';
+import FeedbacksScreen from '../screens/Feedbacks';
+import QuestionsScreen from '../screens/Questions';
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () =>{};
+export default DrawerNavigator;
