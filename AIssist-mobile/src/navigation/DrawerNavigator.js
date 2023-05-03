a99625b64d805ca376a055701d46a0970c3776f6
unknown <mirvatbarakat1@gmail.com>
2023-05-03 22:13:51 +0300

    AIssist-mobile<> Added drawer navigation header style

--- AIssist-mobile/src/navigation/DrawerNavigator.js
+++ AIssist-mobile/src/navigation/DrawerNavigator.js
@@ -4,13 +4,23 @@ import CommunityScreen from '../screens/Community';
 import ActivitiesScreen from '../screens/Activities';
 import FeedbacksScreen from '../screens/Feedbacks';
 import QuestionsScreen from '../screens/Questions';
+import { colors } from '../constants/Palette';
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () =>{
 
     return (
-        <Drawer.Navigator initialRouteName="Community">
+        <Drawer.Navigator initialRouteName="Community"
+         screenOptions={{
+          headerStyle: {
+            backgroundColor: colors.primary_1,
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
           <Drawer.Screen name="Community" component={CommunityScreen} />
           <Drawer.Screen name="Activities" component={ActivitiesScreen} />
           <Drawer.Screen name="Feedbacks" component={FeedbacksScreen} />
