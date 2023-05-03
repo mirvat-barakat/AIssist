4e490b14f4bf819654958149909a8f3a9bd203be
unknown <mirvatbarakat1@gmail.com>
2023-05-03 23:37:10 +0300

    AIssist-mobile<> Updated get specialists function

--- AIssist-mobile/src/screens/Specialists/index.jsx
+++ AIssist-mobile/src/screens/Specialists/index.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './styles';
+import axios from 'axios';
 
 export default function SpecialistsScreen() {
 
     const[specialists, setSpecialists]= useState([]);
-    const token = AsyncStorage.getItem("token");
+    // const token = AsyncStorage.getItem("token");
 
     const getSpecialists = async () => {
         // const token = await AsyncStorage.getItem("token");
@@ -12,14 +13,14 @@ export default function SpecialistsScreen() {
         const config = {
           method: "GET",
           headers: { Authorization: `Bearer ${token}` },
-          url: 'http://192.168.1.6:8000/api/v0.0.1/specialist',
+          url: 'http://192.168.1.6:8000/api/v0.0.1/specialists',
         };
     
         try {
           const res = await axios(config);
     
           if (res.data.status == "success") {
-            setFeed(res.data.posts);
+            setSpecialists(res.data.posts);
             console.log(res.data);
           }
         } catch (error) {
