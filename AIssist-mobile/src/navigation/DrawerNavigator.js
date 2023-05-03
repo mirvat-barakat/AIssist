63125b8186534ea723fa24e7e24d9d31ced3abc5
unknown <mirvatbarakat1@gmail.com>
2023-05-03 23:19:35 +0300

    AIssist-mobile<> Added specialists screen and added it to drawer navigator

--- AIssist-mobile/src/navigation/DrawerNavigator.js
+++ AIssist-mobile/src/navigation/DrawerNavigator.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {View, Image} from "react-native";
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import CommunityScreen from '../screens/Community';
 import ActivitiesScreen from '../screens/Activities';
 import FeedbacksScreen from '../screens/Feedbacks';
 import QuestionsScreen from '../screens/Questions';
+import SpecialistsScreen from '../screens/Specialists';
 import { colors } from '../constants/Palette';
 
 const Drawer = createDrawerNavigator();
@@ -22,9 +24,10 @@ const DrawerNavigator = () =>{
           drawerInactiveTintColor: '#000',
         }}>
           <Drawer.Screen name="Community" component={CommunityScreen} />
+          <Drawer.Screen name="Specialists" component={SpecialistsScreen} />
+          <Drawer.Screen name="Questions" component={QuestionsScreen} />
           <Drawer.Screen name="Activities" component={ActivitiesScreen} />
           <Drawer.Screen name="Feedbacks" component={FeedbacksScreen} />
-          <Drawer.Screen name="Questions" component={QuestionsScreen} />
         </Drawer.Navigator>
       );
 };
---
+++ AIssist-mobile/src/screens/Specialists/index.jsx
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import styles from './styles';
+
+export default function SpecialistsScreen() {};
