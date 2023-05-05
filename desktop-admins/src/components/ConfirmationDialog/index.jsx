deb59d0ae22c4b0c5ba316d183f45eb56f774e6d
unknown <mirvatbarakat1@gmail.com>
2023-05-05 14:24:26 +0300

    AIssist-desktop<>Added confirmation catalog component structure

--- desktop-admins/src/components/ConfirmationDialog/index.jsx
+++ desktop-admins/src/components/ConfirmationDialog/index.jsx
@@ -1,5 +1,18 @@
 import React, {useState} from 'react';
 import './styles.css';
 
-const Confirmation = () => {};
+const Confirmation = () => {
+
+    const { message, onCancel, onConfirm } = props;
+
+    return(
+        <div className="confirmation-dialog">
+            <div className="confirmation-dialog-message">{message}</div>
+            <div>
+                <button className="confirmation-dialog-button-cancel"  onClick={onCancel}>Cancel</button>
+                <button className="confirmation-dialog-button-confirm"  onClick={onConfirm}>Confirm</button>
+            </div>
+        </div>
+    );
+};
 export default Confirmation;
