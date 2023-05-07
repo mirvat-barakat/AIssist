c498ab1e03bde136b4babcf91913efee4f4681af
unknown <mirvatbarakat1@gmail.com>
2023-05-07 23:17:25 +0300

    AIssist-frontend<>Added feedback form files

---
+++ AIssist-mobile/src/components/FeedbackForm/index.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import styles from './styles';
+
+const FeedbackForm = () => {};
+
+export default FeedbackForm;
---
+++ AIssist-mobile/src/components/FeedbackForm/styles.js
@@ -0,0 +1,6 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '../../constants/Palette';
+
+const styles = StyleSheet.create({});
+
+export default styles;
