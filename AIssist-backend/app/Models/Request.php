2aa853a767bfff1090858ff79183cd1335781300
unknown <mirvatbarakat1@gmail.com>
2023-05-07 16:35:41 +0300

    AIssist-backend<>Added request model

---
+++ AIssist-backend/app/Models/Request.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Request extends Model
+{
+    use HasFactory;
+
+    protected $table = 'activity_requests';
+    protected $fillable = [
+        'age',
+        'gender',
+        'diagnosis',
+        'medications',
+        'interests',
+        'notes',
+        'things_have_tried',
+        'user_id',
+    ];
+}
