5a0eb7b2669230c5fcd362731da18bd07343d311
unknown <mirvatbarakat1@gmail.com>
2023-05-08 04:42:54 +0300

    AIssist-frontend<>Displayed activities in feedbacks screen

--- AIssist-mobile/src/screens/Feedbacks/index.jsx
+++ AIssist-mobile/src/screens/Feedbacks/index.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, TextInput, TouchableOpacity, View, SafeAreaView, ScrollView } from 'react-native';
 import styles from './styles';
 import FeedbackForm from '../../components/FeedbackForm';
 
 export default function FeedbacksScreen() {
+    const[activities, setActivities]= useState([]);
 
 
     return(
@@ -12,6 +13,20 @@ export default function FeedbacksScreen() {
                 <Text style={styles.feedbacksText}>Please fill out this form to help us determine  which feedbacks best suits your child case.</Text>
             </View>
             <FeedbackForm/>
+            <View style={styles.generatedActivities}>
+                       {activities.map(activity=> (
+                        <View style={styles.activityView}>
+                        <View key={activity.id}>
+                          <View style={styles.card} >
+                             <View style={styles.specialistInfo}>
+                                <Text style={styles.activityName}>{activity.name}</Text>
+                                <Text style={styles.activityDescription}>{activity.description}</Text>
+                          </View>
+                             </View>
+                          </View> 
+                        </View>
+                       ))}
+            </View>
         </ScrollView>
     )
 };
