e995cb2d934dc2037a41b47777ac46979490ba89
unknown <mirvatbarakat1@gmail.com>
2023-05-10 18:46:37 +0300

    AIssist-mobile<>Added launch screen files

---
+++ AIssist-mobile/src/screens/Launch/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, TouchableOpacity, View, SafeAreaView, Image } from 'react-native';
+import styles from './styles';
+
+export default function LaunchScreen() {
+    return(
+        <ScrollView>
+        </ScrollView>
+    );
+}
