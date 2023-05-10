ffa342fd7d61db56f399fa5204722ec61b36d883
unknown <mirvatbarakat1@gmail.com>
2023-05-10 19:18:01 +0300

    AIssist-mobile<>Finalized launch screen structure and styles

--- AIssist-mobile/src/screens/Launch/index.jsx
+++ AIssist-mobile/src/screens/Launch/index.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { Text, TouchableOpacity, View, SafeAreaView, Image } from 'react-native';
 import styles from './styles';
 
-export default function LaunchScreen() {
+
+export default function LaunchScreen({navigation}) {
     return(
-        <SafeAreaView style={styles.mainViewLaunch}>
-          <Image
-          style={styles.logoLaunch} 
-          source={require('../../../assets/images/Logo.png')}
-            />
-        </SafeAreaView >
-    );
+        <SafeAreaView style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image style={styles.logo} source={require('../../../assets/images/Logo.png')} />
+        </View>
+        <View style={styles.bottomContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
+            <Text style={styles.buttonText}>Let's Get Started</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    )
 }
--- AIssist-mobile/src/screens/Launch/styles.js
+++ AIssist-mobile/src/screens/Launch/styles.js
@@ -2,18 +2,36 @@ import { StyleSheet } from 'react-native';
 import { colors } from '../../constants/Palette';
 
 const styles = StyleSheet.create({
-    mainViewLaunch:{
-        width:'100%',
-        height:'100%',
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
         backgroundColor:colors.primary_1,
-
-    },
-    logoLaunch: {
-        top:'50%',
-        bottom:'50%',
-        height: 170,
-        resizeMode: 'contain',
-        marginTop: -40,
+      },
+      logoContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 50,
+      },
+      logo: {
+        width: 200,
+        height: 200,
+      },
+      bottomContainer: {
+        marginBottom: 50,
+      },
+      button: {
+        backgroundColor: colors.primary_3,
+        paddingVertical: 15,
+        paddingHorizontal: 70,
+        borderRadius: 10,
+      },
+      buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+        alignSelf:'center'
       },
 });
 export default styles;
